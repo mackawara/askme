2a89593c14b8dc8ccb32ef35fb94a8cea7d12ab9
mackawara <mkawara@outlook.com>
2023-06-16 02:54:05 +0200

    exec

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ connectDB().then(async () => {
   const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-      // executablePath: process.env.EXECPATH,
+      executablePath: process.env.EXECPATH,
       handleSIGINT: true,
       headless: true,
       args: [
