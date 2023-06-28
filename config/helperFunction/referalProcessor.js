f380cbc82425ff83fd90dad782337bb4960f63a4
mackawara <mkawara@outlook.com>
2023-06-28 13:42:59 +0200

    feat: added new regex to elevate and referal

--- config/helperFunction/referalProcessor.js
+++ config/helperFunction/referalProcessor.js
@@ -1,7 +1,11 @@
 const usersModel = require("../../models/individualUsers");
-const referalList = async (user) => {
+const referalList = async (msgBody,user) => {
   //user sends a number
+  msgBody.replace(/referal|referral/gi,"").replace(/\s/,"").trim()
   // we validate it as a genuine Zim number
+ if( /^(?:\+263|0)[17]\d{8}$/.test(msgBody)){
+  return "The number is not a valid Zimbabwean mobile number"
+ }
   const users = await usersModel.find().exec();
 
   users.forEach(async (user) => {
