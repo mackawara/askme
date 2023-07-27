f229e0227b1d031dcf1543dc598eef9b21184437
mackawara <mkawara@outlook.com>
2023-07-27 11:40:35 +0200

    fix : increase token limit to 180

--- config/helperFunction/clientOn.js
+++ config/helperFunction/clientOn.js
@@ -30,7 +30,7 @@ const clientOn = async (client, arg1, redisClient, MessageMedia) => {
       const chatID = msg.from;
       const expiryTime = getSecsToMidNight();
 
-      let tokenLimit = 100;
+      let tokenLimit = 180;
 
       const expTime = getSecsToMidNight();
 
@@ -105,7 +105,7 @@ const clientOn = async (client, arg1, redisClient, MessageMedia) => {
               client.sendMessage(me, "New user added  " + chatID);
               client.sendMessage(
                 serialisedNumber,
-                `Hi ${notifyName},thank you for using AskMe, the AI powered virtual study assistant.\n\n*Please Read* As a free user you are limited to 3 requests/messages per 24 hour period.\n Subscribe here https://bit.ly/askME_AI_payment to upgrade to 25 messages per day and longer, more complete messages\n*How to use*\n1. *Simply* ask any question and wait for a response. For example you can ask "Explain the theory of relativity"or \n "Give me a step by step procedure of mounting an engine",if the response is incomplete you can just say "continue". Yes, you can chat to *AskMe* as you would to a human (*a super intelligent, all knowing human*) because *Askme* remembers topics that you talked about for the previous 30 minutes.\n\n What *Askme* cannot do\n1.Provide updates on current events (events after October 2021)\n2.Provide opinions on subjective things,\nWe hope you enjoy using the app. Please avoid making too many requests in short period of time, as this may slow down the app and cause your number to be blocked if warnings are not heeded. If your refer 3 people that eventually become users of AskME you fet additional usage priviledges simply send referal and their number e.g referal 263774111111,  Join our group to stay up to date https://chat.whatsapp.com/I5RNx9PsfYjE0NV3vNijk3 `
+                `Hi ${notifyName},thank you for using AskMe, the AI powered virtual study assistant.\n\n*Please Read* As a free user you are limited to 1 request/message per 24 hour period.\n Subscribe here https://bit.ly/askME_AI_payment to upgrade to 25 messages per day and longer, more complete messages\n*How to use*\n1. *Simply* ask any question and wait for a response. For example you can ask "Explain the theory of relativity"or \n "Give me a step by step procedure of mounting an engine",if the response is incomplete you can just say "continue". Yes, you can chat to *AskMe* as you would to a human (*a super intelligent, all knowing human*) because *Askme* remembers topics that you talked about for the previous 30 minutes.\n\n What *Askme* cannot do\n1.Provide updates on current events (events after October 2021)\n2.Provide opinions on subjective things,\nWe hope you enjoy using the app. Please avoid making too many requests in short period of time, as this may slow down the app and cause your number to be blocked if warnings are not heeded. If your refer 3 people that eventually become users of AskME you fet additional usage priviledges simply send referal and their number e.g referal 263774111111,  Join our group to stay up to date https://chat.whatsapp.com/I5RNx9PsfYjE0NV3vNijk3 `
               );
             } catch (err) {
               client.sendMessage(me, "Save new user failed");
@@ -183,7 +183,7 @@ const clientOn = async (client, arg1, redisClient, MessageMedia) => {
         const isBlocked = await redisClient.hGet(chatID, "isBlocked");
         await redisClient
           .incrBy(`${chatID}shortTTL`, 1)
-          .then(async (result) => {});
+          .then(async (result) => { });
         await redisClient.expire(`${chatID}shortTTL`, 30);
 
         const shortTTL = await redisClient.get(`${chatID}shortTTL`);
@@ -401,9 +401,9 @@ const clientOn = async (client, arg1, redisClient, MessageMedia) => {
         );
         if (
           response ==
-            "Error , request could not be processed, please try again later" ||
+          "Error , request could not be processed, please try again later" ||
           response ==
-            "I can only continue based on previous 3 messages if they were made within the last 3 minutes"
+          "I can only continue based on previous 3 messages if they were made within the last 3 minutes"
         ) {
           redisClient.HINCRBY(chatID, "calls", -1);
           msg.reply(response);
